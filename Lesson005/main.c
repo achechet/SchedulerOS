a32d1d3b5afb850a2b108e12298a14f1418ebac8
achechet <achechet@gmail.com>
2022-10-10 12:43:38 +0300

    Lesson005-2

--- Lesson005/main.c
+++ Lesson005/main.c
@@ -1,11 +1,21 @@
+typedef unsigned long int uint32_t;
+
 #define NVIC_ISER 	(*(volatile long *) 0xE000E100) 		// Interrupt enable set register
 #define NVIC_ICER 	(*(volatile long *) 0xE000E180) 		// Interrupt enable clear register	
 
+void _delay(uint32_t);
+
 int main(void) 
 {
-				NVIC_ISER |= (1<<0);					// 
-				NVIC_ICER |= (1<<0);
+
 	while(1)
 	{
+				NVIC_ISER |= (1<<0);					// 
+				_delay(2000);
+				NVIC_ICER |= (1<<0);
+				_delay(2000);
 	}
 }
+void _delay(uint32_t index) {
+	while(index--);
+	}
