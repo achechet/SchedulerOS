c812c67702d1f83df60a598ab3e841615978a6f3
achechet <achechet@gmail.com>
2022-12-02 12:39:40 +0300

    Lesson009

---
+++ Lesson009/main.c
@@ -0,0 +1,18 @@
+void SysTick_Handler(void);           // SysTick Handler
+
+// External Interrupts
+void Timer0_IRQHandler(void);         // 0: Default
+void Timer1_IRQHandler(void);         // 
+
+void Timer0_IRQHandler(void) 
+	{
+		for(;;);
+	};
+
+int main(void) 
+	{
+		while(1) 
+			{
+			
+			}
+	}
